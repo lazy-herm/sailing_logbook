058f3aa597fe2d5c1357889fd59ad66a6d1e763f
Marc Vautheny <marc@vautheny.com>
2023-01-03 22:55:17 +0000

    changed delete button to delete img and added edit img (without functionality). Next to add edit functionality.

--- src/LogTable.js
+++ src/LogTable.js
@@ -1,16 +1,17 @@
 import { Fragment, useContext } from "react";
-import Button from 'react-bootstrap/Button';
 import AuthContext from "./AuthContext";
 import { deleteData } from "./firebase";
 import Dates from "./Table/Dates";
 import SimpleCell from "./Table/SimpleCell";
-import Vessel from './Table/Vessel';
-import Voyage from './Table/Voyage';
+import Vessel from "./Table/Vessel";
+import Voyage from "./Table/Voyage";
+import editImg from "./imgs/edit-20x20.png";
+import closeImg from './imgs/close-20x20.png';
 
 const LogTable = (props) => {
   const ctx = useContext(AuthContext);
   const deleteHandler = (event) => {
-    deleteData('/logs/'+ctx.isLoggedIn+'/'+event.target.value);
+     deleteData("/logs/" + ctx.isLoggedIn + "/" + event.target.attributes.value.value);
   };
 
   return (
@@ -24,15 +25,28 @@ const LogTable = (props) => {
         Object.entries(props.data).map((entry, index) => {
           return (
             <tr key={index}>
-              <Dates start_date={entry[1]['start date']} end_date={entry[1]['end date']} />
+              <Dates
+                start_date={entry[1]["start date"]}
+                end_date={entry[1]["end date"]}
+              />
               <Vessel data={entry[1]} />
               <Voyage data={entry[1]} />
-              <SimpleCell data={entry[1].days_onboard} unit='day' unitPlural='days' />
+              <SimpleCell
+                data={entry[1].days_onboard}
+                unit="day"
+                unitPlural="days"
+              />
               <SimpleCell data={entry[1].distance} unit="NM" />
-              <SimpleCell data={entry[1].night_hours} unit="hr" unitPLural='hrs' />
+              <SimpleCell
+                data={entry[1].night_hours}
+                unit="hr"
+                unitPLural="hrs"
+              />
               <SimpleCell data={entry[1].skipper_name} />
               <td>
-                <Button onClick={deleteHandler} value={entry[0]}>x</Button>
+                <img src={editImg} alt="edit button" />
+              
+                <img src={closeImg} alt='delete button' onClick={deleteHandler} value={entry[0]} />
               </td>
             </tr>
           );
